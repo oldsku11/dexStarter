427e441c4043060f1b6737331cf041b663897a46
oldsku11 <muradov86@gmail.com>
2023-03-18 11:13:55 +0100

    header implementation.

--- dex/src/App.js
+++ dex/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import "./components/Header";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Dex</div>;
+  return (
+    
+    <div className="App">
+      <Header/>
+    </div>
+
+  )
 }
 
 export default App;
--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Logo from "../moralis-logo.svg"
+import Eth from "../eth.svg"
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='leftH'>
+        <img src={Logo} alt='logo'className='logo' />
+        <div className='headerItem'>Swap</div>
+        <div className='headerItem'>Tokens</div>
+      </div>
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img src={Eth} alt='eth' className='eth' />
+          Ethereum
+        </div>
+        <div className='connectButton'>Connect</div>
+      </div>
+    </header>
   )
 }
 
