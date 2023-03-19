27cfe32d6972f0c212f177749e5be70b33713da8
oldsku11 <muradov86@gmail.com>
2023-03-19 12:51:30 +0100

    added inputs for swap tokens with token tickers and switch button

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -5,12 +5,28 @@ import {
   DownOutlined,
   SettingOutlined,
 } from '@ant-design/icons';
+import tokenList from '../tokenList.json'
 
 function Swap() {
   const [slippage, setSlippage] = useState(2.5);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [tokenOneAmount, setTokenOneAmount] = useState(null);
+  const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
 
   function handleSlippageChange(e){
     setSlippage(e.target.value);
+  }
+
+  function changeAmount(e){
+    setTokenOneAmount(e.target.value);
+  }
+
+  function switchTokens() {
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
   }
 
   const settings = (
@@ -38,6 +54,23 @@ function Swap() {
         >
           <SettingOutlined className='cog'/>
         </Popover>
+      </div>
+      <div className='inputs'>
+        <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} />
+        <Input placeholder='0' value={tokenTwoAmount} disabled={true} />
+        <div className='switchButton' onClick={switchTokens}>
+          <ArrowDownOutlined className='switchArrow' />
+        </div>
+        <div className='assetOne'>
+          <img src={tokenOne.img} alt='assetOneLogo' className='assetLogo' />
+          {tokenOne.ticker}
+          <DownOutlined />
+        </div>
+        <div className='assetTwo'>
+          <img src={tokenTwo.img} alt='assetTwoLogo' className='assetLogo' />
+          {tokenTwo.ticker}
+          <DownOutlined />
+        </div>
       </div>
     </div>
   )
